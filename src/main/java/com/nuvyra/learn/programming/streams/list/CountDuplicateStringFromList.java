f68bf5637d9bce23747a670aa18e9b4dddda6ceb
Abhilash Rupa <abhilashrupa@gmail.com>
2026-01-16 22:26:00 +0530

    format java program to count dulpicate string from list

--- src/main/java/com/nuvyra/learn/programming/streams/list/CountDuplicateStringFromList.java
+++ src/main/java/com/nuvyra/learn/programming/streams/list/CountDuplicateStringFromList.java
@@ -11,26 +11,24 @@ import java.util.stream.Collectors;
 public class CountDuplicateStringFromList {
 
     public static void main(String[] args) {
-                List<String> names = Arrays.asList("a", "b", "d", "a", "a", "b", "z", "z", "b", "b", "b");
+        List<String> names = Arrays.asList("a", "b", "d", "a", "a", "b", "z", "z", "b", "b", "b");
 
-                approach1(names);
-                approach2(names);
+        approach1(names);
+        approach2(names);
 
     }
-
-
 
     private static void approach1(List<String> names) {
         System.out.println("approach 1: using stream functions");
 
         Map<String, Long> duplicateCountMap = names.stream()
-        .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()))
-        .entrySet()
-        .stream()
-        .filter(entry -> entry.getValue()>1)
-        .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()))
+                .entrySet()
+                .stream()
+                .filter(entry -> entry.getValue() > 1)
+                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
 
-        System.out.println("duplicate count map: "+ duplicateCountMap);
+        System.out.println("duplicate count map: " + duplicateCountMap);
 
     }
 
@@ -40,14 +38,13 @@ public class CountDuplicateStringFromList {
         Map<String, Integer> countMap = new HashMap<>();
 
         names.stream()
-        .forEach(value -> countMap.merge(value, 1, Integer::sum));
+                .forEach(value -> countMap.merge(value, 1, Integer::sum));
 
         countMap.entrySet()
-        .stream()
-        .filter(entry ->entry.getValue()>1)
-        .forEach(duplicateEntry -> System.out.println(duplicateEntry.getKey() +" count: "+ duplicateEntry.getValue()));
-        
-
+                .stream()
+                .filter(entry -> entry.getValue() > 1)
+                .forEach(duplicateEntry -> System.out
+                        .println(duplicateEntry.getKey() + " count: " + duplicateEntry.getValue()));
 
     }
 
