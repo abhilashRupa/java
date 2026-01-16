59f26af9fce24df7cd804d39839ad976d194ddd8
Abhilash Rupa <abhilashrupa@gmail.com>
2026-01-16 22:51:08 +0530

    java programm to count the number of string contain with given char

---
+++ src/main/java/com/nuvyra/learn/programming/streams/list/countStringWithSpecifiedCharacter.java
@@ -0,0 +1,46 @@
+package com.nuvyra.learn.programming.streams.list;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+
+/*
+* Given a list of string, write a program to count the number of string containing character 'a' using Java Stream API.
+* */
+
+public class countStringWithSpecifiedCharacter {
+
+    public static void main(String[] args) {
+        List<String> fruitsNames = Arrays.asList("apple", "banana", "orange", "grape", "plum", "kiwi");
+        char givenChar = 'a';
+
+        findOccurrenceUsingCount(fruitsNames, givenChar);
+        findOccurrenceUsingCollect(fruitsNames, givenChar);
+
+    }
+
+    private static void findOccurrenceUsingCount(List<String> fruitsNames, char givenChar) {
+        System.out.println("\napproach 1: using stream and count method");
+
+        long count = fruitsNames.stream()
+                .filter(value -> value.contains(String.valueOf(givenChar)))
+                .count();
+
+        System.out.println("number of string with given character: " + count);
+    }
+
+    private static void findOccurrenceUsingCollect(List<String> fruitsNames, char givenChar) {
+
+        System.out.println("\napproach 2: using collect, toList, size");
+
+        List<String> occurrenceCount = fruitsNames.stream()
+                .filter(value -> value.contains(String.valueOf(givenChar)))
+                .collect(Collectors.toList());
+
+        System.out.println("number of Strings with " + givenChar + " is " + occurrenceCount.size());
+
+    }
+
+}
