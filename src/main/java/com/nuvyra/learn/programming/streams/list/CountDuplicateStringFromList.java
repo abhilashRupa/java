405e20b41aba3548405116572e87aeb66ac2dcdc
Abhilash Rupa <abhilashrupa@gmail.com>
2026-01-16 22:49:47 +0530

    format java program to count dulpicate string from list

--- src/main/java/com/nuvyra/learn/programming/streams/list/CountDuplicateStringFromList.java
+++ src/main/java/com/nuvyra/learn/programming/streams/list/CountDuplicateStringFromList.java
@@ -19,7 +19,7 @@ public class CountDuplicateStringFromList {
     }
 
     private static void approach1(List<String> names) {
-        System.out.println("approach 1: using stream functions");
+        System.out.println("\napproach 1: using stream functions");
 
         Map<String, Long> duplicateCountMap = names.stream()
                 .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()))
@@ -33,7 +33,7 @@ public class CountDuplicateStringFromList {
     }
 
     private static void approach2(List<String> names) {
-        System.out.println("approach 2: using external map and merge");
+        System.out.println("\napproach 2: using external map and merge");
 
         Map<String, Integer> countMap = new HashMap<>();
 
