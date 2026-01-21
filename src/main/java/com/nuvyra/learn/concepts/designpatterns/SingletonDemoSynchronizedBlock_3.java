bb8fd8051c9b22361feb97816f69b95661e6b75d
Abhilash Rupa <abhilashrupa@gmail.com>
2026-01-21 22:01:52 +0530

    java program for synchronozied block singlton class

---
+++ src/main/java/com/nuvyra/learn/concepts/designpatterns/SingletonDemoSynchronizedBlock_3.java
@@ -0,0 +1,52 @@
+package com.nuvyra.learn.concepts.designpatterns;
+
+public class SingletonDemoSynchronizedBlock_3 {
+
+    public static void main(String[] args) {
+        Thread t1 = new Thread(new Runnable() {
+
+            @Override
+            public void run() {
+
+                LogManagerSynchronizedBlock logManagerSynchronizedBlock1 = LogManagerSynchronizedBlock
+                        .getLogManagerSynchronized();
+                System.out.println(logManagerSynchronizedBlock1.hashCode());
+
+            }
+
+        });
+
+        Thread t2 = new Thread(new Runnable() {
+
+            @Override
+            public void run() {
+                LogManagerSynchronizedBlock logManagerSynchronizedBlock2 = LogManagerSynchronizedBlock
+                        .getLogManagerSynchronized();
+                System.out.println(logManagerSynchronizedBlock2.hashCode());
+            }
+        });
+
+        t1.start();
+        t2.start();
+    }
+
+}
+
+class LogManagerSynchronizedBlock {
+
+    private LogManagerSynchronizedBlock() {
+        System.out.println("instance created");
+    }
+
+    private static LogManagerSynchronizedBlock logManagerSynchronizedBlock;
+
+    public static LogManagerSynchronizedBlock getLogManagerSynchronized() {
+        synchronized (LogManagerSynchronizedBlock.class) {
+            if (logManagerSynchronizedBlock == null) {
+                logManagerSynchronizedBlock = new LogManagerSynchronizedBlock();
+            }
+        }
+        return logManagerSynchronizedBlock;
+    }
+
+}
