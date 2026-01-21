aa4ea123d7d2464ec8fb181b8b77a034cf2c5621
Abhilash Rupa <abhilashrupa@gmail.com>
2026-01-21 11:36:03 +0530

    demo example for Lazy initialization  Singleton class

---
+++ src/main/java/com/nuvyra/learn/concepts/designpatterns/SingletonDemoLazy_2.java
@@ -0,0 +1,31 @@
+package com.nuvyra.learn.concepts.designpatterns;
+
+public class SingletonDemoLazy_2 {
+    public static void main(String[] args) {
+        LogManagerDemoLazy logManagerDemoLazy1 = LogManagerDemoLazy.getLogManagerDemo();
+        System.out.println(logManagerDemoLazy1.hashCode());
+
+        LogManagerDemoLazy logManagerDemoLazy2 = LogManagerDemoLazy.getLogManagerDemo();
+        System.out.println(logManagerDemoLazy2.hashCode());
+        
+    }
+
+}
+
+class LogManagerDemoLazy {
+
+    private static LogManagerDemoLazy logManagerDemoLazy;
+
+    private LogManagerDemoLazy(){
+        System.out.println("instance created.");
+    }
+
+    public static LogManagerDemoLazy getLogManagerDemo(){
+        if(logManagerDemoLazy == null) {
+            logManagerDemoLazy = new LogManagerDemoLazy();    
+        }
+        return logManagerDemoLazy;
+        
+    }
+
+}
