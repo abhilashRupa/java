7801764282a51e17db742390a163f8b8727e08d5
Abhilash Rupa <abhilashrupa@gmail.com>
2026-01-27 22:56:11 +0530

    new approach to reverse the 2nd string in array

--- src/main/java/com/nuvyra/learn/programming/interview/ReverseSecondString.java
+++ src/main/java/com/nuvyra/learn/programming/interview/ReverseSecondString.java
@@ -20,34 +20,51 @@ public class ReverseSecondString {
         bestApproach(input);
 
         approach1(input);
+        approach2(input);
 
     }
 
     private static void bestApproach(String input) {
+        System.out.println("\n java 8 approach");
 
         String[] inputArr = input.split("\\s");
-        
-       String output = IntStream.range(0, inputArr.length)
-        .mapToObj(i -> i == 1 
-            ? new StringBuffer(inputArr[i]).reverse()
-            : inputArr[i]
-        ).collect(Collectors.joining(" "));
+
+        String output = IntStream.range(0, inputArr.length)
+                .mapToObj(i -> i == 1
+                        ? new StringBuffer(inputArr[i]).reverse()
+                        : inputArr[i])
+                .collect(Collectors.joining(" "));
 
         System.out.println(output);
 
     }
 
     private static void approach1(String input) {
+        System.out.println("\n java 8 hardcode approach ");
+
 
         List<String> inputList = Arrays.asList(input.split("\\s"));
 
-        String output =inputList.stream()
-        .map(s -> s.equalsIgnoreCase("coding") ? new StringBuilder(s).reverse() : s)
-        .collect(Collectors.joining(" "));
+        String output = inputList.stream()
+                .map(s -> s.equalsIgnoreCase("coding") ? new StringBuilder(s).reverse() : s)
+                .collect(Collectors.joining(" "));
 
         System.out.println(output);
 
+    }
 
+    private static void approach2(String input) {
+        System.out.println("\n simple java approach");
+        String[] inputArr = input.split("\\s");
+
+        if(inputArr.length>=2){
+            inputArr[1] = new StringBuilder(inputArr[1]).reverse().toString();
+        }
+
+        System.out.println(String.join(" ", inputArr));
+
+
+        
     }
 
 }
